859e5870947658c7070bc03213a1057287121b8a
marcbailo <mbailo@institutmvm.cat>
2025-03-21 19:39:09 +0100

    creamos una variable y le asignamos 20, este serán el num de elementos visibles por cada página en nuestro crud.  Luego con un if definimos el parametro de nuestra url para indicar en qué pagina estamos, si no indicamos nada en la url por defecto nos mandará a la 1, es decir, la primera de todas. Añadimos la conexión con el require a nuestra base de datos.

--- crud/productos.php
+++ crud/productos.php
@@ -1,12 +1,43 @@
+<?php
+
+
+      $filasmax = 20;
+
+    if (isset($_GET['pag']))
+        {
+        $pagina = $_GET['pag'];
+    } else
+        {
+        $pagina = 1;        
+    } 
+    
+// Conexión a la base de datos
+require('connection.php'); 
+
+?>                  
+
+
+
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CRUD de productos</title>
+    <meta charset="UTF-8">
 </head>
 <body>
- <!-- Tabla de productos -->
+    
+<!-- Tabla de productos -->
  <table>
     <thead>
         <tr>

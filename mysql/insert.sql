3acc0ae608348b68aae46407fced1398aacf2bba
marcbailo <mbailo@institutmvm.cat>
2025-03-21 20:25:56 +0100

    he creado unn form para filtrar las categorias, he puesto unas cateorias de ejemplo, más adelante añadiremos la función de filtrar, he incluido un botón.

--- mysql/insert.sql
+++ mysql/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO productos ('id','nombre','descripcion','stock','percio','tipo_ENUM') VALUES 
+INSERT INTO productos ('id','nombre','descripcion','stock','precio','tipo_ENUM') VALUES 
 (001, 'producto1', 'descripcion1', 5, 1.99,  FISICO),
 (002, 'producto2', 'descripcion2', 10, 4.99, FISICO),
 (003, 'producto4', 'descripcion3', 20, 6.99, DIGITAL),

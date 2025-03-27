b28c5fa3b8b16d7776b6eec0282663ea976afaa5
bixiPRO <bixiangzhu6666@gmail.com>
2025-03-27 17:35:27 +0100

    cambiar de email per gmail

--- WEB/mail.php
+++ WEB/mail.php
@@ -15,7 +15,7 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.email.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'soporte.magiang@gmail.com';                     //SMTP username
     $mail->Password   = 'kkyy ojhu xlaj alup';                               //SMTP password

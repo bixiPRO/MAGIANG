d98181eb54a2a68577d1d448686763393ca477ec
bixiPRO <bixiangzhu6666@gmail.com>
2025-03-31 19:55:13 +0200

    crear un php para que el usuario meta formulario de datos

---
+++ WEB/info_pago.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<head>
+    <title> FORMPAGOS</title>
+    <favicon href="img/favicon.">
+    <link rel="stylesheet" type="text/css" href="css/style_home.css">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <encode utf-8>
+</head>

e1c14f7e058bd019621bde38d1b37d607a6d47ff
bixiPRO <bixiangzhu6666@gmail.com>
2025-04-02 15:33:31 +0200

    hacer que los campos para poner el mail y la contrasenya sean obligatorias, lo hemos hecho en el login.php pero por si hacen un salo lo ponemos tambien en este .php para registrar

--- WEB/do_register.php
+++ WEB/do_register.php
@@ -2,8 +2,15 @@
 session_start();
 require('connection.php');
 
-$email = $_POST['gmail'];
-$password1 = $_POST['pwd1'];
-$password2 = $_POST['pwd2'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['gmail'];
+    $password1 = $_POST['pwd1'];
+    $password2 = $_POST['pwd2'];
 
+    if (empty($email) || empty($password1) || empty($password2)) {
+        $_SESSION['error'] = "Todos los campos son obligatorios";
+        header("Location: login.php");
+        exit();
+    }
+}
 ?>

c204e73b688331dee457498efce642e2c4d05de8
marcbailo <mbailo@institutmvm.cat>
2025-04-04 19:36:50 +0200

    método para borrar los productos de nuestra base de datos. Hacemos que pille la id de nuestros productos, solo pueden ser enteros y que se puedan borrar por id.

---
+++ crud/del_producto.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+require('connection.php');
+
+if (isset($_POST['id'])) {
+    $id = (int) $_POST['id'];
+
+    // Eliminar el campeón de la base de datos
+    $stmt = $conn->prepare("DELETE FROM productos WHERE id = ?");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $stmt->close();
+}
+
+header("Location: productos.php");
+

be405e2ac350d63e95302beaaac2c8c9b6b6e0ee
bixiPRO <bixiangzhu6666@gmail.com>
2025-05-06 14:58:32 +0200

    anadir insert de plataformas y categorias

--- mysql/insert.sql
+++ mysql/insert.sql
@@ -1,6 +1,8 @@
-INSERT INTO productos ('id','nombre','categoria','stock','precio','formato') VALUES 
-(001, 'producto1', 'categoria1', 5, 1.99,  FISICO),
-(002, 'producto2', 'categoria2', 10, 4.99, FISICO),
-(003, 'producto4', 'categoria3', 20, 6.99, DIGITAL),
-(004, 'producto5', 'categoria4', 65, 8.99, DIGITAL),
-(005, 'producto6', 'categoria5', 15, 9.99, FISICO);
+INSERT INTO plataformas (nombre) VALUES
+('PC'), ('Apple'), ('Google Play'), ('Xbox'), ('Playstation'), ('Switch');
+
+INSERT INTO categorias (nombre) VALUES
+('Teclado'), ('Raton'), ('Auriculares'), ('Alfombrilla'),
+('Micrófonos'), ('Mobil'), ('Apple'), ('Android'), 
+('Tablet'), ('Monitores'), ('Portátiles'), ('Ordenadores'),
+('Gaming'), ('Oficina');

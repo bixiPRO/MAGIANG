d9e45cf123c0227b47269b3f29603650be2e2617
bixiPRO <bixiangzhu6666@gmail.com>
2025-05-06 15:47:22 +0200

    mejor creamos una tabla temporal para distribuir las plataformas y categorias de cada producto de forma eficiente

--- mysql/insertar_productos.sql
+++ mysql/insertar_productos.sql
@@ -1,6 +1,16 @@
+CREATE TABLE temp_productos (
+    nombre VARCHAR(255),
+    descripcion VARCHAR(255),
+    tipo ENUM('FISICO', 'DIGITAL'),
+    precio DECIMAL(10,2),
+    stock INT DEFAULT 0,
+    categoria VARCHAR(255),
+    plataforma VARCHAR(255)
+);
+
 LOAD DATA INFILE '~/Documents/MAGIANG/mysql/productos_informaticos.csv' 
-INTO TABLE productos
+INTO TEMPORARY TABLE temp_productos
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"' 
 LINES TERMINATED BY '\n'
-(nombre, descripcion, tipo, precio, stock);
+(nombre, descripcion, tipo, precio, stock, categoria, plataforma);

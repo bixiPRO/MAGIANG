a89dacfa8984631831f9207a484299db47275e2e
bixiPRO <bixiangzhu6666@gmail.com>
2025-05-08 15:24:50 +0200

    ambiar para poder introduir la informacion a la bbdd

--- WEB/seleccion_pago.php
+++ WEB/seleccion_pago.php
@@ -1,11 +1,12 @@
 <?php 
     session_start();
+    require('connection.php');
     // La seccio per coger la información del formulario de la pagina pago.php
-    $_SESSION['pedido'] = [
-        'nombre' => $_POST['nombre'],
-        'apellidos' => $_POST['apellidos'],
-        'direccion' => $_POST['direccion'],
-        'telefono' => $_POST['telefono'],
-        'email' => $_POST['email']
-    ];
+    $nombre = $_POST['nombre'];
+    $apellidos = $_POST['apellidos'];
+    $direccion = $_POST['direccion'];
+    $telefono = $_POST['telefono'];
+    $email = $_POST['email'];
+
+
 ?>

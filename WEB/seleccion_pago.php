fd1e3e51cb3c7ed4f0c5aecf8858303546dfbc50
bixiPRO <bixiangzhu6666@gmail.com>
2025-05-08 16:54:51 +0200

    poner directamente el capcha en seleccion_pago.php

--- WEB/seleccion_pago.php
+++ WEB/seleccion_pago.php
@@ -11,6 +11,41 @@
     $codigo_postal = $_POST['codigo_postal'];
     $direccion = $_POST['direccion'];
     $puerta = $_POST['puerta'];
+?>
+
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title> Pago - Magiang </title>
+    <favicon href="img/favicon.">
+    <link rel="stylesheet" type="text/css" href="css/pago-styles.css">
+    <encode utf-8>
+</head>
+
+<body>
+    <form action="seleccion_pago.php" method="post">
+        <h3>CAPCHA: </h3>
+        <h5>Introduce el mail que has registrado de tu cuenta para la comprovación</h5> </br></br>
+        <label>Correo electronico:</label>
+        <input type="email" id="email" name="email" required><br><br>
+    </form>
+
+    <footer>
+        <div>
+            <img class="icon2" src="img/logo_magiang.png" alt="Imagen"> 
+            <a class="footertitlelogo">Magiang</a>
+        </div>
+
+        <p>©2025</p>
+        <p>Todos los derechos reservados. </p>
+        <p>Descubre las mejores ofertas i compra al mejor precio con nuestra plataforma.</p>
+        <p>Da el salto a nuevos mundos con Magiang</p>
+        <p>¡Contáctanos en desde el apartado de <a href="contacto.html">Contacto</a> en nuestra web!</p>
+    </footer>
+</body>
+</html>
+
+<?php
     $email = $_POST['email']
 
 

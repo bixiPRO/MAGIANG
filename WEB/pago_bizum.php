4d8ad9aa8a2c8e5e05c3d57d5ffb5564eb366592
bixiPRO <bixiangzhu6666@gmail.com>
2025-05-12 15:19:49 +0200

    guardar

---
+++ WEB/pago_bizum.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>PAGO BIZUM</title>
+</head>
+<body>
+
+    <main>
+        <h2> <a>PAGO BIZUM</a></h2>
+        <form method="POST">
+            
+            <label>Pon tu numero de telefono:</label>
+            <input type="number" id="telefono" name="telefono" required><br><br>
+            <a class="boton-pay" href="mail.php">Pagar</a>
+        </form>
+    
+    </main>
+
+</body>
+</html>

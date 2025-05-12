aad1bebace9f1e98286bebb31dc8b2f5879106be
bixiPRO <bixiangzhu6666@gmail.com>
2025-05-12 20:04:45 +0200

    guardar

--- WEB/pago_paypal.php
+++ WEB/pago_paypal.php
@@ -2,16 +2,20 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>PAGO BIZUM</title>
+    <title>PAGO PAYPAL</title>
 </head>
 <body>
 
     <main>
-        <h2> <a>PAGO BIZUM</a></h2>
-        <form method="POST">
+        <h2> <a>PAGO PAYPAL</a></h2>
+        <form action=pago_paypal.php method="POST">
             
-            <label>Pon tu numero de telefono:</label>
-            <input type="number" id="telefono" name="telefono" required><br><br>
+            <label>Pon tu correo electronico:</label>
+            <input type="email" id="email" name="email" required><br><br>
+
+            <label>Contrasenya:</label>
+            <input type="password" id="contrasenya" name="contrasenya" required><br><br>
+
             <a class="boton-pay" href="mail.php">Pagar</a>
         </form>
     
@@ -19,3 +23,27 @@
 
 </body>
 </html>
+
+<?php
+session_start();
+require('connection.php');
+
+$id_cliente = $_SESSION['id_cliente'];
+$password1 = $_POST['contrasenya'];
+
+if (strlen($password1) < 6) {
+    header("Location: pago_paypal.php");
+    exit();
+}
+
+$hash = password_hash($password1,PASSWORD_DEFAULT);
+
+$stmt = $conn->prepare("");
+$stmt->bind_param("sss", $email, $username, $hash);
+$stmt->execute();
+
+$stmt->close();
+$conn->close();
+
+echo "Pago finalizado";
+?>

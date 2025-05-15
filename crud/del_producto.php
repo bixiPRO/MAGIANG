8b0c70f6eb66e9269724fcf5b49d841d40fbbc16
bixiPRO <bixiangzhu6666@gmail.com>
2025-05-16 00:42:49 +0200

    para eliminar un producto tendremos que eliminar estos id_productos primeros ya que son referencias almazenadas en una lista y un foreach para eliminar cada relacion de las tablas con la lista

--- crud/del_producto.php
+++ crud/del_producto.php
@@ -3,8 +3,27 @@ session_start();
 
 require('connection.php');
 
+
 if (isset($_POST['id'])) {
     $id = (int) $_POST['id'];
+    //para eliminar un producto tendremos que eliminar estos id_productos primeros ya que son referencias
+    $tablas = [
+        ['tabla' => 'carrito', 'columna' => 'id_producto'],
+        ['tabla' => 'cupones', 'columna' => 'id_producto'],
+        ['tabla' => 'reseñas', 'columna' => 'id_producto'],
+        ['tabla' => 'pro_cat', 'columna' => 'id'],
+        ['tabla' => 'pro_pla', 'columna' => 'id'],
+        ['tabla' => 'digital', 'columna' => 'id_producto'],
+        ['tabla' => 'ventas', 'columna' => 'id_producto'],
+    ];
+    //un foreach para eliminar cada relacion de las tablas con la lista
+    foreach ($tablas as $tabla) {
+        $sql = "DELETE FROM {$tabla['tabla']} WHERE {$tabla['columna']} = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $stmt->close();
+    }
 
     // Eliminar el producto de la base de datos
     $stmt = $conn->prepare("DELETE FROM productos WHERE id = ?");
@@ -12,7 +31,7 @@ if (isset($_POST['id'])) {
     $stmt->execute();
     $stmt->close();
 }
-
+$conn->close();
 header("Location: productos.php");
 exit();
 ?>

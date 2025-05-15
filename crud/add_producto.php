387db1acf58f0d6e571f12285e245fed836e4b91
bixiPRO <bixiangzhu6666@gmail.com>
2025-05-16 00:48:29 +0200

    coger el html del form_update_productos.php y ponerlo en add_producto.php

--- crud/add_producto.php
+++ crud/add_producto.php
@@ -9,11 +9,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $precio = $_POST['precio'];
     $stock = $_POST['stock'];
     $tipo = $_POST['tipo'];
-    
+    $imagen = '';
 
     //Insertamos los productos en la base de datos
-    $stmt = $conn->prepare("INSERT INTO productos (nombre, descripcion, precio, stock, tipo, imagen) VALUES (?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssdis",$nombre, $descripcion, $precio, $stock, $tipo);
+    $stmt = $conn->prepare("INSERT INTO productos (nombre, descripcion, precio, stock, tipo, imagen) VALUES (?, ?, ?, ?, ?, ?d)");
+    $stmt->bind_param("ssdis",$nombre, $descripcion, $precio, $stock, $tipo, $imagen);
     $stmt->execute();
     $stmt->close();
 
@@ -25,29 +25,37 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Añadir Producto</title>
+    <title>Modificar Producto</title>
+    
 </head>
 <body>
-    <h1>Añadir Producto</h1>
-    <form action="add_producto.php" method="POST">
-        Nombre: <input type="text" name="nombre" required><br/>
-        Categoria: 
-        <select name="descripcion" required>
-            <option value="Ratones">Ratones</option>
-            <option value="Teclados">Teclados</option>
-            <option value="Ordenadores">Ordenadores</option>
-            <option value="Microfonos">Microfonos</option>
-            <option value="Portatiles">Portatiles</option>
-            <option value="Monitores">Monitores</option>
-        </select><br/>
-        Precio:<input type="number" name="precio" placeholder="1.0" step="0.01" min="0" max="100000" required><br/>
-        Stock: <input type="number" name="stock" required><br/>
-        Formato: 
+    <h1>Modificar Producto</h1>
+    <!-- formulario de la modificacion del producto el selected para opciones en php-->
+    <form method="POST" action="modificar_productos.php">
+        <input type="hidden" name="id" value="<?= $id ?>"/>
+
+        <label>Nombre:</label>
+        <input type="text" name="nombre" value="<?= htmlspecialchars($producto['nombre']) ?>" required><br/>
+
+        <label>Descripción:</label>
+        <input type="text" name="descripcion" value="<?= htmlspecialchars($producto['descripcion']) ?>" required><br/>
+        <!-- step para poner decimales poner en php como monedas reales-->
+        <label>Precio:</label>
+        <input type="number" name="precio" value="<?= htmlspecialchars($producto['precio']) ?>" step="0.01" min="0" required><br/>
+
+        <label>Stock:</label>
+        <input type="number" name="stock" value="<?= htmlspecialchars($producto['stock']) ?>" min="0" required><br/>
+
+        <label>Directorio Imagen:</label>
+        <input type="text" name="imagen" value="<?= htmlspecialchars($producto['imagen']) ?>"><br/>
+
         <select name="tipo" required>
-            <option value="Fisico">Físico</option>
-            <option value="Digital">Digital</option>
+            <option value="FISICO" <?= $producto['tipo'] == 'FISICO' ? 'selected' : '' ?>>Físico</option>
+            <option value="DIGITAL" <?= $producto['tipo'] == 'DIGITAL' ? 'selected' : '' ?>>Digital</option>
         </select><br/>
-        <input type="submit" value="Añadir Producto">
+
+        <input type="submit" value="Modificar Producto">
+
     </form>
 </body>
 </html>

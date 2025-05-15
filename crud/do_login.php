0de29ba0e445b924ca5920dff374209a717e13b0
bixiPRO <bixiangzhu6666@gmail.com>
2025-05-15 17:19:05 +0200

    cambiar la tabla para que cada email sea unico, he usado el login del magiang para admin_magiang sobre el login

---
+++ crud/do_login.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+require('connection.php');
+
+// Validar que los datos vengan por POST
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $email = $_POST['gmail'] ?? '';
+    $password1 = $_POST['pwd'] ?? '';
+
+    // Verificar que no estén vacíos
+    if (empty($email) || empty($password1)) {
+        $_SESSION['error_login'] = "Campos obligatorios.";
+        header("Location: login.php");
+        exit();
+    }
+
+$stmt = $conn->prepare('SELECT id, email, contrasenya FROM administradores WHERE email = ?;');
+$stmt->bind_param("s",$email);
+$stmt->execute();
+$resultado = $stmt->get_result();
+$fila = $resultado->fetch_assoc();
+
+if ($fila && $password1=$fila['contrasenya']){
+	echo "login correcto";
+	$_SESSION['gmail']=$email;
+	$_SESSION['nombre_usuario'] = $fila['nombre_usuario'];
+	header("Location: productos.php");
+	exit();
+
+}else {
+	echo "login incorrecto";
+	header("Location: login.php");
+	exit();
+}
+$stmt->close();
+$conn->close();
+}else {
+    header("Location: login.php");
+    exit();
+}
+?>

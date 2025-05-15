aeeaabf5c6bde3ce8a2f35dd4403965902edbd8b
bixiPRO <bixiangzhu6666@gmail.com>
2025-05-16 00:54:42 +0200

    quitar values porque esto es para anadir y tambien quitar el id ya que la tabla es autoincrement

--- crud/add_producto.php
+++ crud/add_producto.php
@@ -7,31 +7,30 @@
 </head>
 <body>
     <h1>Modificar Producto</h1>
-    <!-- formulario de la modificacion del producto el selected para opciones en php-->
+    <!-- formulario para anadir-->
     <form method="POST">
-        <input type="hidden" name="id" value="<?= $id ?>"/>
 
         <label>Nombre:</label>
-        <input type="text" name="nombre" value="<?= htmlspecialchars($producto['nombre']) ?>" required><br/>
+        <input type="text" name="nombre" required><br/>
 
         <label>Descripción:</label>
-        <input type="text" name="descripcion" value="<?= htmlspecialchars($producto['descripcion']) ?>" required><br/>
+        <input type="text" name="descripcion" required><br/>
         <!-- step para poner decimales poner en php como monedas reales-->
         <label>Precio:</label>
-        <input type="number" name="precio" value="<?= htmlspecialchars($producto['precio']) ?>" step="0.01" min="0" required><br/>
+        <input type="number" name="precio" step="0.01" min="0" required><br/>
 
         <label>Stock:</label>
-        <input type="number" name="stock" value="<?= htmlspecialchars($producto['stock']) ?>" min="0" required><br/>
+        <input type="number" name="stock" min="0" required><br/>
 
         <label>Directorio Imagen:</label>
-        <input type="text" name="imagen" value="<?= htmlspecialchars($producto['imagen']) ?>"><br/>
+        <input type="text" name="imagen" ><br/>
 
         <select name="tipo" required>
             <option value="FISICO" <?= $producto['tipo'] == 'FISICO' ? 'selected' : '' ?>>Físico</option>
             <option value="DIGITAL" <?= $producto['tipo'] == 'DIGITAL' ? 'selected' : '' ?>>Digital</option>
         </select><br/>
 
-        <input type="submit" value="Modificar Producto">
+        <input type="submit" value="Anadir Producto">
 
     </form>
 </body>
@@ -51,12 +50,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $imagen = '';
 
     //Insertamos los productos en la base de datos
-    $stmt = $conn->prepare("INSERT INTO productos (nombre, descripcion, precio, stock, tipo, imagen) VALUES (?, ?, ?, ?, ?, ?d)");
+    $stmt = $conn->prepare("INSERT INTO productos (nombre, descripcion, precio, stock, tipo, imagen) VALUES (?, ?, ?, ?, ?, ?)");
     $stmt->bind_param("ssdis",$nombre, $descripcion, $precio, $stock, $tipo, $imagen);
     $stmt->execute();
     $stmt->close();
 
     header("Location: productos.php");
+    
 
 }
 ?>

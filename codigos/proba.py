1ea89a6f8e5f3f2efd2819a565b87b58453c87ad
bixiPRO <bixiangzhu6666@gmail.com>
2025-05-16 17:52:21 +0200

    eliminar aquest .py de proba

--- codigos/proba.py
+++
@@ -1,6 +0,0 @@
-import codigos
-
-nombre_producto,codigos_generados = codigos.main()
-
-for codigo in codigos_generados:
-        print(f"INSERT INTO codigos(nombre_producto, codigo) VALUES ('{nombre_producto}', '{codigo}');")

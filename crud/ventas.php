8f3db936ee59a5711da63253f2e4600a7549b3c0
bixiPRO <bixiangzhu6666@gmail.com>
2025-05-16 22:06:07 +0200

    no ha cambiado mucho haciendo en ventas

--- crud/ventas.php
+++ crud/ventas.php
@@ -39,35 +39,41 @@ if (!isset($_SESSION['id'])) {
         <main>
             <h1> Ventas Realizadas </h1>
             <table>
-                <tr>
-                <th>Producto</th>
-                <th>Cantidad</th>
-                <th>Precio producto</th>
-                <th>Total</th>
-                </tr>
-                <?php
-                    // query para obtener datos necesssarios para saber cuantos productos han comprado
-                    $query = "SELECT p.nombre AS producto, 
-                            v.numeros AS cantidad,
-                            p.precio AS precio,
-                            (v.numeros * p.precio) AS precio_total
-                        FROM ventas v
-                        JOIN productos p ON v.id_producto = p.id;";
-                    $resultat= mysqli_query($conn, $query);
-                    // mostrar los resultados
-                    if ($resultat && $resultat->num_rows > 0) {
-                        while ($row = $resultat->fetch_assoc()) {
-                            echo "<tr>";
-                            echo "<td>" . htmlspecialchars($row['producto']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['cantidad']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['precio']) . " €</td>";
-                            echo "<td>" . htmlspecialchars($row['precio_total']) . " €</td>";
-                            echo "</tr>";
+                <thead>
+                    <tr>
+                    <th>Producto</th>
+                    <th>Cantidad</th>
+                    <th>Precio producto</th>
+                    <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        // query para obtener datos necesssarios para saber cuantos productos han comprado
+                        $query = "SELECT p.nombre AS producto, 
+                                v.numeros AS cantidad,
+                                p.precio AS precio,
+                                (v.numeros * p.precio) AS precio_total
+                            FROM ventas v
+                            JOIN productos p ON v.id_producto = p.id;";
+                        $resultat= mysqli_query($conn, $query);
+                        // mostrar los resultados
+                        if ($resultat && $resultat->num_rows > 0) {
+                            while ($row = $resultat->fetch_assoc()) {
+                                echo '<div class="S_content-item">';
+                                echo "<tr>";
+                                echo "<td>" . htmlspecialchars($row['producto']) . "</td>";
+                                echo "<td>" . htmlspecialchars($row['cantidad']) . "</td>";
+                                echo "<td>" . htmlspecialchars($row['precio']) . " €</td>";
+                                echo "<td>" . htmlspecialchars($row['precio_total']) . " €</td>";
+                                echo "</tr>";
+                                echo "</div>";
+                            }
+                        }else {
+                            echo "<p>No hay ventas en registro.</p>";
                         }
-                    }else {
-                        echo "<p>No hay ventas en registro.</p>";
-                    }
-                ?>
+                    ?>
+                </tbody>
             </table>
         </main>
     </body>
